b0d9d36a7a392378519c842bdf387fec1d4cfa5b
Emmanuel Garcia <emmanuel.garcia@gnp.com.mx>
2018-02-05 14:24:08 -0600

    Code splitting with react router, check react-router version

--- src/router.js
+++ src/router.js
@@ -3,20 +3,55 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import Home from './components/Home';
 import ArtistMain from './components/artists/ArtistMain';
-import ArtistDetail from './components/artists/ArtistDetail';
-import ArtistCreate from './components/artists/ArtistCreate';
-import ArtistEdit from './components/artists/ArtistEdit';
+
+/*
+  IMPORTANTE:
+  No generar una funcion que ayude a hacer el código siguiente
+  mas corto, es decir un helper y ayudar a que el código sea
+  mas legible, esto NO se debe hacer porque Webpack lee el codigo
+  de forma estatica linea por linea buscando System.import's y
+  no puede usar concatenacion o interpolacion (plantillas de cadenas)
+  para hacer uso del code splitting, dejarlo asi.
+
+  Tambien recomienda usarlo solo en caso de que tengamos muchas rutas,
+  y obviamente en caso de que esas rutas no sean consultadas frecuentemente
+  y que por lo tanto no se requeiran siempre con cada ingreso a la aplicación.
+
+  Otro detalle, dejar solamente en el import la principal o los componentes
+  mas utilizados, como el login por ejemplo o la pagina inicial.
+*/
+const componentRoutes = {
+  component: Home,
+  path: '/',
+  indexRoute: { component: ArtistMain },
+  childRoutes:[
+    {
+      path: 'artists/new',
+      getComponent(location, cb){
+        System.import('./components/artists/ArtistCreate')
+        .then(module => cb(null, module.default));
+      }
+    },
+    {
+      path: 'artists/:id',
+      getComponent(location, cb){
+        System.import('./components/artists/ArtistDetail')
+        .then(module => cb(null, module.default));
+      }
+    },
+    {
+      path: 'artists/:id/edit',
+      getComponent(location, cb){
+        System.import('./components/artists/ArtistEdit')
+        .then(module => cb(null, module.default));
+      }
+    }
+  ]
+};
 
 const Routes = () => {
   return (
-    <Router history={hashHistory}>
-      <Route path="/" component={Home}>
-        <IndexRoute component={ArtistMain} />
-        <Route path="artists/new" component={ArtistCreate} />
-        <Route path="artists/:id" component={ArtistDetail} />
-        <Route path="artists/:id/edit" component={ArtistEdit} />
-      </Route>
-    </Router>
+    <Router history={hashHistory} routes={componentRoutes}/>
   );
 };
 
