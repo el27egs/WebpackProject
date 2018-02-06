ae45531f641658567bbe9a680d890b0d125139ed
Emmanuel Garcia <emmanuel.garcia@gnp.com.mx>
2018-02-05 20:39:32 -0600

    Adding deploy script

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Routes from './router';
 import '../style/materialize.css';
 import '../style/react-range.css';
 import '../style/style.css';
-console.log('Hi there!!');
+console.log('github.com!!');
 const App = () => {
   const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
 
